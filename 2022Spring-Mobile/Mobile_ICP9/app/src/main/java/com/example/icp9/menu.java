172cf9b6768efaaf3dae21422ea140cd1fa7b544
Joe Moon <moonjose@gmail.com>
2022-03-27 13:38:52 -0300

    ICP 9 report

--- 2022Spring-Mobile/Mobile_ICP9/app/src/main/java/com/example/icp9/menu.java
+++ 2022Spring-Mobile/Mobile_ICP9/app/src/main/java/com/example/icp9/menu.java
@@ -1,7 +1,6 @@
 package com.example.icp9;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -10,7 +9,6 @@ import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.NumberPicker;
 import android.widget.Toast;
-
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 
