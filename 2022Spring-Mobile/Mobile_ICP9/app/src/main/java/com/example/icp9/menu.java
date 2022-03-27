27e6a2b427354e486d04eae4553b1bc9b58d0e3f
Joe Moon <moonjose@gmail.com>
2022-03-26 22:01:25 -0300

    ICP 9 - Summary Activity implemented.

--- 2022Spring-Mobile/Mobile_ICP9/app/src/main/java/com/example/icp9/menu.java
+++ 2022Spring-Mobile/Mobile_ICP9/app/src/main/java/com/example/icp9/menu.java
@@ -4,13 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.NumberPicker;
 import android.widget.Toast;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 
 public class menu extends AppCompatActivity {
@@ -21,15 +22,11 @@ public class menu extends AppCompatActivity {
     final double TAX_RATE = 1.07;
     double subtotal = 0.0;
 
-//    EditText customername = findViewById(R.id.name);
-//    CheckBox pepperoni = (CheckBox) findViewById(R.id.pepperoni);
-//    CheckBox cheese = (CheckBox) findViewById(R.id.cheese);
-//    CheckBox mushroom = (CheckBox) findViewById(R.id.mushroom);
-//    CheckBox gpepper = (CheckBox) findViewById(R.id.gpepper);
-//    CheckBox olives = (CheckBox) findViewById(R.id.olives);
-//    CheckBox sausage = (CheckBox) findViewById(R.id.sausage);
+    EditText customername;
+    String pizza_size;
 
-    ArrayList<String> toppings;
+    private NumberPicker quantityPicker;
+    final ArrayList<String> toppings = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,12 +39,16 @@ public class menu extends AppCompatActivity {
         Button summary = findViewById(R.id.summary);
         Button order = findViewById(R.id.order);
 
-        toppings = new ArrayList<>();
+        customername= findViewById(R.id.name);
+        quantityPicker = findViewById(R.id.quantity);
+        quantityPicker.setMinValue(1);
+        quantityPicker.setMaxValue(99);
 
         small.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 subtotal = SMALL_PRICE;
+                pizza_size = "Small";
                 Toast.makeText(menu.this, "Small selected.", Toast.LENGTH_SHORT).show();
             }
         });
@@ -56,6 +57,7 @@ public class menu extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 subtotal = MEDIUM_PRICE;
+                pizza_size = "Medium";
                 Toast.makeText(menu.this, "Medium selected.", Toast.LENGTH_SHORT).show();
             }
         });
@@ -64,6 +66,7 @@ public class menu extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 subtotal = LARGE_PRICE;
+                pizza_size = "Large";
                 Toast.makeText(menu.this, "Large selected.", Toast.LENGTH_SHORT).show();
             }
         });
@@ -72,24 +75,38 @@ public class menu extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(menu.this, summary.class);
-                intent.putExtra("price", calculateTotal(subtotal));
+                intent.putExtra("name", customername.toString());
+                intent.putExtra("quantity", quantityPicker.getValue());
+                intent.putExtra("size", pizza_size);
                 intent.putStringArrayListExtra("toppings", getToppings());
-                Toast.makeText(menu.this, TextUtils.join(", ", getToppings()), Toast.LENGTH_SHORT).show();
-               // startActivity(intent);
+                intent.putExtra("price", calculateTotal(subtotal));
+                startActivity(intent);
             }
         });
 
         order.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Toast.makeText(menu.this, "ORDER", Toast.LENGTH_SHORT).show();
             }
         });
     }
 
+    public ArrayList<String> getToppings() {
+        return toppings;
+    }
+
+    public String calculateTotal(double subtotal) {
+        DecimalFormat priceFormat = new DecimalFormat("0.00");
+        return priceFormat.format(subtotal * TAX_RATE * quantityPicker.getValue());
+    }
+
     public void onCheckboxClicked(View view) {
+        // Is the view now checked?
         boolean checked = ((CheckBox) view).isChecked();
-        switch (view.getId()) {
+
+        // Check which checkbox was clicked
+        switch(view.getId()) {
             case R.id.pepperoni:
                 if (checked)
                     toppings.add("Pepperoni");
@@ -102,54 +119,33 @@ public class menu extends AppCompatActivity {
                 else
                     toppings.remove(toppings.indexOf("Extra Cheese"));
                 break;
+            case R.id.mushroom:
+                if (checked)
+                    toppings.add("Mushrooms");
+                else
+                    toppings.remove(toppings.indexOf("Mushrooms"));
+                break;
+            case R.id.gpepper:
+                if (checked)
+                    toppings.add("Green Peppers");
+                else
+                    toppings.remove(toppings.indexOf("Green Peppers"));
+                    break;
+            case R.id.olives:
+                if (checked)
+                    toppings.add("Olives");
+                else
+                    toppings.remove(toppings.indexOf("Olives"));
+                break;
+            case R.id.sausage:
+                if (checked)
+                    toppings.add("Sausage");
+                else
+                    toppings.remove(toppings.indexOf("Sausage"));
+                break;
+            default:
+                Toast.makeText(menu.this, "ERROR: toppings not registering", Toast.LENGTH_SHORT).show();
+                break;
         }
-    }
-
-    public ArrayList<String> getToppings() {
-        return toppings;
-    }
-
-//    public ArrayList<String> getToppings(View view) {
-//        toppings = new ArrayList<>();
-//        boolean checked = ((CheckBox) view).isChecked();
-//
-//        switch(view.getId()) {
-//            case R.id.pepperoni:
-//                if (checked)
-//                    toppings.add("Pepperoni");
-//                else
-//                    toppings.remove(toppings.indexOf("Pepperoni"));
-//                break;
-//            case R.id.cheese:
-//                if (checked)
-//                    toppings.add("Extra Cheese");
-//                else
-//                    toppings.remove(toppings.indexOf("Extra Cheese"));
-//                break;
-//        }
-//        if (pepperoni.isChecked()) {
-//            toppings.add("Pepperoni");
-//        }
-//        if (cheese.isChecked()) {
-//            toppings.add("Extra Cheese");
-//        }
-//        if (olives.isChecked()) {
-//            toppings.add("Olives");
-//        }
-//        if (mushroom.isChecked()) {
-//            toppings.add("Mushroom");
-//        }
-//        if (gpepper.isChecked()) {
-//            toppings.add("Green Peppers");
-//        }
-//        if (sausage.isChecked()) {
-//            toppings.add("Sausage");
-//        }
-
-//        return toppings;
-//    }
-
-    public double calculateTotal(double subtotal) {
-        return subtotal * TAX_RATE;
     }
 }
--- 2022Spring-Mobile/Mobile_ICP9/app/src/main/java/com/example/icp9/summary.java
+++ 2022Spring-Mobile/Mobile_ICP9/app/src/main/java/com/example/icp9/summary.java
@@ -1,34 +1,46 @@
 package com.example.icp9;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
-
+import android.widget.TextView;
 import java.util.ArrayList;
 
 public class summary extends AppCompatActivity {
 
-    ListView lv = (ListView) findViewById(R.id.toppings_list);
-    //ArrayList<String> toppings = new ArrayList<String>();
+    private String customer;
+    private String size;
+    private int quantity;
+    private ListView topping_display;
+    private String total;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_summary);
 
-        Bundle extras = getIntent().getExtras();
-        if (extras != null) {
-            double price = extras.getDouble("price");
-            String price_str = String.format("%.2f", price);
-            Toast.makeText(summary.this, price_str, Toast.LENGTH_SHORT).show();
-            ArrayList<String> toppings = extras.getStringArrayList("toppings");
-            ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, toppings);
-            lv.setAdapter(arrayAdapter);
-        }
+        TextView quantity_size = findViewById(R.id.quantity_size);
 
+        Intent intent = getIntent();
+        customer = intent.getStringExtra("customer");
+        size = intent.getStringExtra("size");
+        quantity = intent.getIntExtra("quantity", 0);
+        total = intent.getStringExtra("price");
 
+        quantity_size.setText("You ordered " + quantity + " " + size + " pizzas.");
+
+        topping_display = (ListView) findViewById(R.id.toppings_list);
+        ArrayList<String> topping_array = intent.getStringArrayListExtra("toppings");
+        ArrayAdapter<String> topping_adapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                topping_array
+        );
+        topping_display.setAdapter(topping_adapter);
+
+        TextView orderTotal = findViewById(R.id.total);
+        orderTotal.setText("Order Total: $" + total);
     }
 }
