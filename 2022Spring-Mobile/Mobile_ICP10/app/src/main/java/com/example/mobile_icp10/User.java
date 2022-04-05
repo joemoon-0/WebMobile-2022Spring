e31961c6b841bee20c6dc2093f752c8b5684cd16
Joe Moon <moonjose@gmail.com>
2022-04-04 21:07:08 -0500

    ICP 10 program

---
+++ 2022Spring-Mobile/Mobile_ICP10/app/src/main/java/com/example/mobile_icp10/User.java
@@ -0,0 +1,19 @@
+package com.example.mobile_icp10;
+
+import com.google.gson.annotations.SerializedName;
+
+public class User {
+
+    private int id;
+
+    @SerializedName("login")
+    private String userName;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+}
